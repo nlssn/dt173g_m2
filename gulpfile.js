b72400491790367ac0a1cf0cced6079bb54e826a
Johannes Nilsson <johannes@nilsson.im>
2020-08-28 17:38:46 +0200

    Add styles task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 //const gulp = require('gulp');
 const { src, dest, parallel } = require('gulp');
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
 const terser = require('gulp-terser');
 const imagemin = require('gulp-imagemin');
 
@@ -8,8 +12,16 @@ function html() {
    return src('src/*.html').pipe(dest('dist'));
 }
 
+function styles() {
+   return src('src/css/**/*.css')
+      .pipe(concat('global.css'))
+      .pipe(postcss([autoprefixer(), cssnano()]))
+      .pipe(dest('dist/css'));
+}
+
 function scripts() {
-   return src('src/js/script.js')
+   return src('src/js/**/*.js')
+      .pipe(concat('global.js'))
       .pipe(terser())
       .pipe(dest('dist/js'));
 }
@@ -20,4 +32,4 @@ function images() {
       .pipe(dest('dist/images'));
 }
 
-exports.default = parallel(html,scripts, images);
+exports.default = parallel(html,styles, scripts, images);
